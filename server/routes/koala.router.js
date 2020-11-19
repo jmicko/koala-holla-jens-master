55d420ddae0d553e8df89a87ddb052d5158f5ca3
Jordan Newberry <jordan.newberry@gmail.com>
2020-11-19 10:54:51 -0600

    added GET route

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,11 +1,21 @@
+
 const express = require('express');
-const koalaRouter = express.Router();
+const router = express.Router();
 
 // DB CONNECTION
-
+const pool = require('../modules/pool');
 
 // GET
-
+router.get('/', (req, res) => {
+    let sqlText = `SELECT * FROM koala;`;
+    pool.query(sqlText)
+        .then(result => {
+            res.send(result.rows);
+        }).catch(error => {
+            console.log('error getting books', error);
+            res.sendStatus(500);
+        });
+})
 
 // POST
 
@@ -15,4 +25,4 @@ const koalaRouter = express.Router();
 
 // DELETE
 
-module.exports = koalaRouter;
+module.exports = router;
